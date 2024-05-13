c7875fbc741b11c4d075b555d6f928268dbc3e1d
mdemelop <maria.paulino@usp.br>
2024-05-13 17:26:53 -0300

    Last exercises

---
+++ HFpart/Exercise1/taskMini.cxx
@@ -0,0 +1,335 @@
+// Copyright 2019-2020 CERN and copyright holders of ALICE O2.
+// See https://alice-o2.web.cern.ch/copyright for details of the copyright holders.
+// All rights not expressly granted are reserved.
+//
+// This software is distributed under the terms of the GNU General Public
+// License v3 (GPL Version 3), copied verbatim in the file "COPYING".
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+/// \file taskMini.cxx
+/// \brief Mini version of the D0 analysis chain
+///
+/// \author Vít Kučera <vit.kucera@cern.ch>, Inha University
+
+// O2
+#include "CommonConstants/PhysicsConstants.h"
+#include "DCAFitter/DCAFitterN.h"
+#include "Framework/AnalysisDataModel.h"
+#include "Framework/AnalysisTask.h"
+#include "Framework/HistogramRegistry.h"
+#include "Framework/runDataProcessing.h"
+
+// O2Physics
+#include "Common/Core/RecoDecay.h"
+#include "Common/Core/TrackSelectorPID.h"
+#include "Common/Core/trackUtilities.h"
+#include "Common/DataModel/PIDResponse.h"
+
+// PWGHF
+#include "PWGHF/Core/HfHelper.h"
+#include "Tutorials/PWGHF/DataModelMini.h"
+
+using namespace o2;
+using namespace o2::aod;
+using namespace o2::framework;
+using namespace o2::framework::expressions;
+
+// Candidate creation =====================================================================
+
+/// Candidate creator
+/// Reconstruction of heavy-flavour 2-prong decay candidates
+struct HfCandidateCreator2Prong {
+  Produces<aod::HfCandProng2Base> rowCandidateBase;
+
+  // vertexing parameters
+  Configurable<double> magneticField{"magneticField", 5., "magnetic field [kG]"};
+  Configurable<bool> propagateToPCA{"propagateToPCA", true, "create tracks version propagated to PCA"};
+  Configurable<bool> useAbsDCA{"useAbsDCA", false, "Minimise abs. distance rather than chi2"};
+  Configurable<double> maxR{"maxR", 200., "reject PCA's above this radius"};
+  Configurable<double> maxDZIni{"maxDZIni", 4., "reject (if>0) PCA candidate if tracks DZ exceeds threshold"};
+  Configurable<double> minParamChange{"minParamChange", 1.e-3, "stop iterations if largest change of any X is smaller than this"};
+  Configurable<double> minRelChi2Change{"minRelChi2Change", 0.9, "stop iterations if chi2/chi2old > this"};
+
+  o2::vertexing::DCAFitterN<2> fitter; // 2-prong vertex fitter
+  double massPiK{0.};
+  double massKPi{0.};
+
+  using TracksWithCov = soa::Join<Tracks, TracksCov>;
+
+  OutputObj<TH1F> hMass{TH1F("hMass", "2-prong candidates;inv. mass (#pi K) (GeV/#it{c}^{2});entries", 500, 0., 5.)};
+
+  void init(InitContext&)
+  {
+    // Configure the vertexer
+    fitter.setBz(magneticField);
+    fitter.setPropagateToPCA(propagateToPCA);
+    fitter.setMaxR(maxR);
+    fitter.setMaxDZIni(maxDZIni);
+    fitter.setMinParamChange(minParamChange);
+    fitter.setMinRelChi2Change(minRelChi2Change);
+    fitter.setUseAbsDCA(useAbsDCA);
+  }
+
+  void process(aod::Collisions const&,
+               aod::HfTrackIndexProng2 const& rowsTrackIndexProng2,
+               TracksWithCov const&)
+  {
+    // loop over pairs of track indices
+    for (const auto& rowTrackIndexProng2 : rowsTrackIndexProng2) {
+      auto track0 = rowTrackIndexProng2.prong0_as<TracksWithCov>();
+      auto track1 = rowTrackIndexProng2.prong1_as<TracksWithCov>();
+      auto trackParVarPos1 = getTrackParCov(track0);
+      auto trackParVarNeg1 = getTrackParCov(track1);
+      auto collision = track0.collision();
+
+      // reconstruct the 2-prong secondary vertex
+      if (fitter.process(trackParVarPos1, trackParVarNeg1) == 0) {
+        continue;
+      }
+      const auto& secondaryVertex = fitter.getPCACandidate();
+      auto trackParVar0 = fitter.getTrack(0);
+      auto trackParVar1 = fitter.getTrack(1);
+
+      // get track momenta
+      std::array<float, 3> pVec0;
+      std::array<float, 3> pVec1;
+      trackParVar0.getPxPyPzGlo(pVec0);
+      trackParVar1.getPxPyPzGlo(pVec1);
+
+      // fill candidate table rows
+      rowCandidateBase(collision.globalIndex(),
+                       collision.posX(), collision.posY(), collision.posZ(),
+                       secondaryVertex[0], secondaryVertex[1], secondaryVertex[2],
+                       pVec0[0], pVec0[1], pVec0[2],
+                       pVec1[0], pVec1[1], pVec1[2],
+                       rowTrackIndexProng2.prong0Id(), rowTrackIndexProng2.prong1Id());
+
+      // fill histograms
+      // calculate invariant masses
+      auto arrayMomenta = std::array{pVec0, pVec1};
+      massPiK = RecoDecay::m(arrayMomenta, std::array{o2::constants::physics::MassPiPlus, o2::constants::physics::MassKPlus});
+      massKPi = RecoDecay::m(arrayMomenta, std::array{o2::constants::physics::MassKPlus, o2::constants::physics::MassPiPlus});
+      hMass->Fill(massPiK);
+      // hMass->Fill(massKPi);
+    }
+  }
+};
+
+/// Helper extension task
+/// Extends the base table with expression columns (see the HfCandProng2Ext table).
+struct HfCandidateCreator2ProngExpressions {
+  Spawns<aod::HfCandProng2Ext> rowCandidateProng2;
+  void init(InitContext const&) {}
+};
+
+// Candidate selection =====================================================================
+
+/// D0 candidate selector
+struct HfCandidateSelectorD0 {
+  Produces<aod::HfSelCandidateD0> hfSelD0Candidate;
+
+  Configurable<double> ptCandMin{"ptCandMin", 0., "Lower bound of candidate pT"};
+  Configurable<double> ptCandMax{"ptCandMax", 50., "Upper bound of candidate pT"};
+  // TPC
+  Configurable<double> ptPidTpcMin{"ptPidTpcMin", 0.15, "Lower bound of track pT for TPC PID"};
+  Configurable<double> ptPidTpcMax{"ptPidTpcMax", 5., "Upper bound of track pT for TPC PID"};
+  Configurable<double> nSigmaTpc{"nSigmaTpc", 3., "Nsigma cut on TPC only"};
+  // topological cuts
+  Configurable<double> cpaMin{"cpaMin", 0.98, "Min. cosine of pointing angle"};
+  Configurable<double> dlenMin{"dlenMin", 0.01, " Min. decay length"}; //Exercise 1
+  Configurable<double> massWindow{"massWindow", 0.4, "Half-width of the invariant-mass window"};
+
+  HfHelper hfHelper;
+  TrackSelectorPi selectorPion;
+  TrackSelectorKa selectorKaon;
+
+  using TracksWithPid = soa::Join<Tracks,
+                                  aod::pidTPCFullPi, aod::pidTPCFullKa,
+                                  aod::pidTOFFullPi, aod::pidTOFFullKa>;
+
+  void init(InitContext const&)
+  {
+    selectorPion.setRangePtTpc(ptPidTpcMin, ptPidTpcMax);
+    selectorPion.setRangeNSigmaTpc(-nSigmaTpc, nSigmaTpc);
+    selectorKaon = selectorPion;
+  }
+
+  /// Conjugate-independent topological cuts
+  /// \param candidate is candidate
+  /// \return true if candidate passes all cuts
+  template <typename T>
+  bool selectionTopol(const T& candidate)
+  {
+    // check that the candidate pT is within the analysis range
+    if (candidate.pt() < ptCandMin || candidate.pt() >= ptCandMax) {
+      return false;
+    }
+    // cosine of pointing angle
+    if (candidate.cpa() < cpaMin) {
+      return false;
+    }
+    // decay length - Exercise 1
+    if (candidate.decayLength() < dlenMin){
+      return false;
+    }
+
+
+    return true;
+  }
+
+  /// Conjugate-dependent topological cuts
+  /// \param candidate candidate
+  /// \param trackPion the track with the pion hypothesis
+  /// \param trackKaon the track with the kaon hypothesis
+  /// \note trackPion = positive and trackKaon = negative for D0 selection and inverse for D0bar
+  /// \return true if candidate passes all cuts for the given conjugate
+  template <typename T1, typename T2>
+  bool selectionTopolConjugate(const T1& candidate, const T2& trackPion, const T2& trackKaon)
+  {
+    // invariant-mass cut
+    if (trackPion.sign() > 0) {
+      if (std::abs(hfHelper.invMassD0ToPiK(candidate) - o2::constants::physics::MassD0) > massWindow) {
+        return false;
+      }
+    } else {
+      if (std::abs(hfHelper.invMassD0barToKPi(candidate) - o2::constants::physics::MassD0) > massWindow) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void process(aod::HfCandProng2 const& candidates,
+               TracksWithPid const&)
+  {
+    // looping over 2-prong candidates
+    for (const auto& candidate : candidates) {
+
+      // final selection flag: 0 - rejected, 1 - accepted
+      int statusD0 = 0;
+      int statusD0bar = 0;
+
+      auto trackPos = candidate.prong0_as<TracksWithPid>(); // positive daughter
+      auto trackNeg = candidate.prong1_as<TracksWithPid>(); // negative daughter
+
+      // conjugate-independent topological selection
+      if (!selectionTopol(candidate)) {
+        hfSelD0Candidate(statusD0, statusD0bar);
+        continue;
+      }
+
+      // conjugate-dependent topological selection for D0
+      bool topolD0 = selectionTopolConjugate(candidate, trackPos, trackNeg);
+      // conjugate-dependent topological selection for D0bar
+      bool topolD0bar = selectionTopolConjugate(candidate, trackNeg, trackPos);
+
+      if (!topolD0 && !topolD0bar) {
+        hfSelD0Candidate(statusD0, statusD0bar);
+        continue;
+      }
+
+      // track-level PID selection
+      int pidTrackPosKaon = selectorKaon.statusTpcOrTof(trackPos);
+      int pidTrackPosPion = selectorPion.statusTpcOrTof(trackPos);
+      int pidTrackNegKaon = selectorKaon.statusTpcOrTof(trackNeg);
+      int pidTrackNegPion = selectorPion.statusTpcOrTof(trackNeg);
+
+      int pidD0 = -1;
+      int pidD0bar = -1;
+
+      if (pidTrackPosPion == TrackSelectorPID::Accepted &&
+          pidTrackNegKaon == TrackSelectorPID::Accepted) {
+        pidD0 = 1; // accept D0
+      } else if (pidTrackPosPion == TrackSelectorPID::Rejected ||
+                 pidTrackNegKaon == TrackSelectorPID::Rejected ||
+                 pidTrackNegPion == TrackSelectorPID::Accepted ||
+                 pidTrackPosKaon == TrackSelectorPID::Accepted) {
+        pidD0 = 0; // exclude D0
+      }
+
+      if (pidTrackNegPion == TrackSelectorPID::Accepted &&
+          pidTrackPosKaon == TrackSelectorPID::Accepted) {
+        pidD0bar = 1; // accept D0bar
+      } else if (pidTrackPosPion == TrackSelectorPID::Accepted ||
+                 pidTrackNegKaon == TrackSelectorPID::Accepted ||
+                 pidTrackNegPion == TrackSelectorPID::Rejected ||
+                 pidTrackPosKaon == TrackSelectorPID::Rejected) {
+        pidD0bar = 0; // exclude D0bar
+      }
+
+      if (pidD0 == 0 && pidD0bar == 0) {
+        hfSelD0Candidate(statusD0, statusD0bar);
+        continue;
+      }
+
+      if ((pidD0 == -1 || pidD0 == 1) && topolD0) {
+        statusD0 = 1; // identified as D0
+      }
+      if ((pidD0bar == -1 || pidD0bar == 1) && topolD0bar) {
+        statusD0bar = 1; // identified as D0bar
+      }
+
+      hfSelD0Candidate(statusD0, statusD0bar);
+    }
+  }
+};
+
+// Analysis task =====================================================================
+
+/// D0 analysis task
+struct HfTaskD0 {
+  Configurable<int> selectionFlagD0{"selectionFlagD0", 1, "Selection flag for D0"};
+  Configurable<int> selectionFlagD0bar{"selectionFlagD0bar", 1, "Selection flag for D0 bar"};
+
+  HfHelper hfHelper;
+
+  Partition<soa::Join<aod::HfCandProng2, aod::HfSelCandidateD0>> selectedD0Candidates = aod::hf_selcandidate_d0::isSelD0 >= selectionFlagD0 || aod::hf_selcandidate_d0::isSelD0bar >= selectionFlagD0bar;
+
+  HistogramRegistry registry{
+    "registry",
+    {}};
+
+  void init(InitContext&)
+  {
+    const TString strTitle = "D^{0} candidates";
+    const TString strPt = "#it{p}_{T} (GeV/#it{c})";
+    const TString strEntries = "entries";
+
+    AxisSpec ptAxis = {100,0.,10.}; //Exercise 1
+
+    registry.add("hPtCand", strTitle + ";" + strPt + ";" + strEntries, {HistType::kTH1F, {{100, 0., 10.}}});
+    registry.add("hMass", strTitle + ";" + "inv. mass (#pi K) (GeV/#it{c}^{2})" + ";" + strEntries, {HistType::kTH1F, {{500, 0., 5.}}});
+    registry.add("hCpaVsPtCand", strTitle + ";" + "cosine of pointing angle" + ";" + strPt + ";" + strEntries, {HistType::kTH2F, {{110, -1.1, 1.1}, {100, 0., 10.}}});
+    //Exercise 1
+    registry.add("hDlenVsPtCand", strTitle + ";" + "decay length" + ";" + strPt + ";" + strEntries, {HistType::kTH2F, {{150, 0., 0.1}, {ptAxis}}});
+  }
+
+  void process(soa::Join<aod::HfCandProng2, aod::HfSelCandidateD0> const& candidates)
+  {
+    for (const auto& candidate : selectedD0Candidates) {
+      if (candidate.isSelD0() >= selectionFlagD0) {
+        registry.fill(HIST("hMass"), hfHelper.invMassD0ToPiK(candidate));
+      }
+      if (candidate.isSelD0bar() >= selectionFlagD0bar) {
+        registry.fill(HIST("hMass"), hfHelper.invMassD0barToKPi(candidate));
+      }
+      registry.fill(HIST("hPtCand"), candidate.pt());
+      registry.fill(HIST("hCpaVsPtCand"), candidate.cpa(), candidate.pt());
+      //Exercise 1
+      registry.fill(HIST("hDlenVsPtCand"), candidate.decayLength(), candidate.pt());
+    }
+  }
+};
+
+// Add all tasks in the workflow specification.
+WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
+{
+  return WorkflowSpec{
+    adaptAnalysisTask<HfCandidateCreator2Prong>(cfgc),
+    adaptAnalysisTask<HfCandidateCreator2ProngExpressions>(cfgc),
+    adaptAnalysisTask<HfCandidateSelectorD0>(cfgc),
+    adaptAnalysisTask<HfTaskD0>(cfgc)};
+}
